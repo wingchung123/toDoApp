92de56e8ff23ec36fc2428eb483fb7badf018721
Wing Chow <wingchungus@gmail.com>
2022-11-06 17:26:50 -0500

    init testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello World')
+export function add(num: string): number {
+	let integers = num.split(',')
+	console.log(integers)
+	let integers_array = .map(x => parseInt(x));
+	console.log(integersyyyyy)
+	console.log(integers)
+	return integers.reduce((a,b) => a+b, 0);
+}
+
+console.log(`Total sum: ${ add("") }`)
---
+++ tests/unit-tests/index.test.ts
@@ -0,0 +1,8 @@
+import {add} from '../../src/index';
+
+
+describe('testing index file', () => {
+  test('empty string should result in zero', () => {
+    expect(add('')).toBe(0);
+  });
+});
